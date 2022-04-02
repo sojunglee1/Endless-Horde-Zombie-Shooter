3dc46e75dc6aff25ac875f471433cf29ef5752f4
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-04-02 00:20:59 -0400

    Week 12 - Lab Submission
    - added UI for inventory & pickups
    - need UI functionality for inventory
    - added main menu (simple)
    - added items to pick up for inventory

--- EndlessZombieHordeShooter/Assets/Scripts/SpawnerVolume.cs
+++ EndlessZombieHordeShooter/Assets/Scripts/SpawnerVolume.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnerVolume : MonoBehaviour
 {
-    BoxCollider boxCollider;
+    public BoxCollider boxCollider;
     // Start is called before the first frame update
     void Start()
     {
